dde745f417ea44a3aa9e1baf06c9d4b65a401912
Udimile Macedo <udimile@gmail.com>
2021-11-30 16:41:01 -0300

    exercicio-operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,41 @@
+/**
+ 1. 
+
+
+ 2.
+
+
+ 3.
+
+
+ */
+
+ /* 1. 
+ idadeUsuario = prompt ("Qual a sua idade?")
+ idadeMelhorAmigo = prompt ("Qual a idade do seu(a) melhor amigo(a)?" )
+
+ console.log("Sua idade é maior do que a do seu(a) melhor amigo(a)?", Number(idadeUsuario) > Number(idadeMelhorAmigo))
+ console.log(Number(idadeUsuario)- Number(idadeMelhorAmigo)) 
+
+  2.
+ 
+ numPar = prompt("Insira um número par.")
+ console.log(Number(numPar) % 2)
+
+ c) O padrão é que sempre o resto é zero.
+ d) Há o padrão de sempre sobrar 1. 
+
+
+ 3.
+idadeUsuarioAnos = prompt("Qual sua idade?")
+console.log("A idade em meses é: ", Number(idadeUsuarioAnos)*12 ," meses.")
+console.log("A idade em dias é: ", Number(idadeUsuarioAnos)*365 ," dias.")
+console.log("A idade em horas é: ", (Number(idadeUsuarioAnos)*365)*24 ," horas.")
+
+// 4. 
+num1=prompt("Me diga um número.")
+num2=prompt("Me diga outro número.")
+console.log(Number(num1)>Number(num2));
+console.log(Number(num1)==Number(num2));*/
+console.log(Number(num1)Number(num2)); 
+console.log(Number(num1)>Number(num2));
