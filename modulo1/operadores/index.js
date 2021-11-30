c25b9cab1456dada86baf909d539a6785b6c68fd
Udimile Macedo <udimile@gmail.com>
2021-11-30 17:15:41 -0300

    Exercicio-operadores

--- modulo1/operadores/index.js
+++ modulo1/operadores/index.js
@@ -1,11 +1,18 @@
 /**
- 1. 
+ 1. a. false
+    b. false
+    c. true
+    d. boolean
+    
+ 2. Ele não converteu o que chega no prompt para number .
 
 
- 2.
+ 3. let primeiroNumero = prompt("Digite um numero!")
+    let segundoNumero = prompt("Digite outro numero!")
 
+    const soma = Number(primeiroNumero) + Number(segundoNumero)
 
- 3.
+    console.log(soma)
 
 
  */
@@ -35,7 +42,12 @@ console.log("A idade em horas é: ", (Number(idadeUsuarioAnos)*365)*24 ," horas.
 // 4. 
 num1=prompt("Me diga um número.")
 num2=prompt("Me diga outro número.")
-console.log(Number(num1)>Number(num2));
-console.log(Number(num1)==Number(num2));*/
-console.log(Number(num1)Number(num2)); 
-console.log(Number(num1)>Number(num2));
+console.log("O primeiro número é maior que o segundo? ", Number(num1)>Number(num2));
+console.log("O primeiro número é igual ao segundo? ",Number(num1)==Number(num2));
+console.log("O primeiro número é divisível pelo segundo? ",(Number(num1)%Number(num2))== 0);
+console.log("O segundo número é divisível pelo primeiro? ",(Number(num2)%Number(num1))== 0);
+*/
+
+
+//Desafio
+
