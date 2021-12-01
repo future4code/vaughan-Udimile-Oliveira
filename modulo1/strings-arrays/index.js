680e8169caa91b1530cf8ac06e582d945dd5b449
Udimile Macedo <udimile@gmail.com>
2021-12-01 16:49:04 -0300

    strings-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,46 @@
+//Exercicio de interpretação
+//1.
+//
+//
+//
+//
+// 2.Console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+// SUBI NUM ÔNIBUS EM MIRROCOS 27 
+
+
+// Exercicio de escrita
+/*/1.
+
+let nomeDoUsuario = prompt("Qual o seu nome?")
+let emailDoUsuario = prompt("Qual seu email?")
+
+ 
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario} !`);
+*/
+// 2. 
+
+/*const comidasPreferidas = 
+["Pizza", 
+"strogonoff",
+"sequilhos",
+"batata frita",
+"hamburger"]
+
+console.log(comidasPreferidas);
+
+console.log(`Essas são as minhas comidas preferidas:${comidasPreferidas}`)
+
+//let comidaUsuario = prompt("Digite uma comida.")
+comidasPreferidas.splice(1, 1)
+console.log(comidasPreferidas)
+*/
+//3. 
+const listaDeTarefas = []
+
+listaDeTarefas.push (prompt("Digite uma tarefa diária?"),
+prompt("Digite uma tarefa diária?"),
+prompt("Digite uma tarefa diária?")
+)
+console.log(listaDeTarefas)
+
+
