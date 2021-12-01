2ce2f9562196c91133032e2ea4dc35c9d5a95796
Udimile Macedo <udimile@gmail.com>
2021-12-01 19:53:57 -0300

    strings-arrays

--- modulo1/strings-arrays/index.js
+++ modulo1/strings-arrays/index.js
@@ -1,9 +1,10 @@
 //Exercicio de interpretação
-//1.
-//
-//
-//
-//
+//1.a. indefinido
+// b. null
+// c. 11
+// d. 3
+// e. [3, 19, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// f. 9
 // 2.Console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
 // SUBI NUM ÔNIBUS EM MIRROCOS 27 
 
@@ -17,30 +18,27 @@ let emailDoUsuario = prompt("Qual seu email?")
  
 console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario} !`);
 */
-// 2. 
-
-/*const comidasPreferidas = 
-["Pizza", 
-"strogonoff",
-"sequilhos",
-"batata frita",
-"hamburger"]
-
-console.log(comidasPreferidas);
-
-console.log(`Essas são as minhas comidas preferidas:${comidasPreferidas}`)
-
-//let comidaUsuario = prompt("Digite uma comida.")
-comidasPreferidas.splice(1, 1)
+/*/ 2.
+let comidasPreferidas = [ 'strogonoff', 'batata frita', 'hamburger', 'sequilhos', 'lasanha']
 console.log(comidasPreferidas)
-*/
-//3. 
-const listaDeTarefas = []
+const mensagem = "Essas são as minhas comidas preferidas: "
+console.log(mensagem, comidasPreferidas[0],'\n', mensagem, comidasPreferidas[1],'\n', mensagem, comidasPreferidas[2],'\n', mensagem, comidasPreferidas[3],'\n', mensagem, comidasPreferidas[4])
 
-listaDeTarefas.push (prompt("Digite uma tarefa diária?"),
-prompt("Digite uma tarefa diária?"),
-prompt("Digite uma tarefa diária?")
-)
+let comidaUsuario = prompt("Qual a sua comida preferida?")
+
+comidasPreferidas.splice(1, 1, comidaUsuario)
+
+console.log(comidasPreferidas)    
+
+// 3. 
+let listaDeTarefas = []
+listaDeTarefas.push(
+    prompt("Digite uma tarefa diária"),
+    prompt("Digite mais uma tarefa diária"),
+    prompt("Digite mais uma tarefa diária")
+    )
 console.log(listaDeTarefas)
-
+let tarefaRealizada = prompt("Digite o índice das suas tarefas realizadas: 0, 1 ou 2")
+listaDeTarefas.splice(tarefaRealizada, 1)
+console.log(listaDeTarefas)*/
 
