88c71322a39dd383890a28e0fd218300a9561e42
udimile <76756821+udimile@users.noreply.github.com>
2022-01-17 20:14:42 -0300

    tá na mesma

---
+++ modulo2/rend-condicional-form/rend-condicional-form/src/components/PerguntaAberta.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class PerguntaAberta extends React.Component {
+    render() {
+        return (
+            <div>
+           
+                <p>{this.pergunta}</p>
+                <input></input>
+                
+            </div>
+        )
+    }
+}
