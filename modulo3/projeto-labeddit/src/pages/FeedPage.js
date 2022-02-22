5f09819ee4c383903976bf6c3ad8e3161837b836
udimile <76756821+udimile@users.noreply.github.com>
2022-02-22 17:01:37 -0300

    inicio do feed page

--- modulo3/projeto-labeddit/src/pages/FeedPage.js
+++ modulo3/projeto-labeddit/src/pages/FeedPage.js
@@ -1,9 +1,43 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { urlBase } from "../constants/urlBase"
 import { useProtect } from "../Hooks/useProtect"
 
 export const FeedPage = () => {
+ 
     useProtect()
+ 
+    const [posts, setPosts] = useState({})
+ 
+    const getPosts = () => {
+        const token = window.localStorage.getItem("token")
     
-    return(
-        <div>Oi</div>
+        const headers = {
+            headers: {
+                Authorization:  token 
+            }
+        }
+            axios.get(`${urlBase}/posts`, headers)
+                .then(res => {
+                    setPosts(res.data)
+                })
+                .catch(err => {
+                    console.log(err.response)
+                })
+    }
+
+    useEffect(() => { getPosts() }, [])
+
+    return (
+        <div>
+            <div>
+                <p></p>
+            </div>
+            <div></div>
+            <div>
+
+            </div>
+
+        </div>
     )
 }
