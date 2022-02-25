2c3fc95bf35bcb9c50321549d0a31a1f74be3d29
udimile <76756821+udimile@users.noreply.github.com>
2022-02-25 16:56:15 -0300

    quase, mas falta muito

--- modulo3/projeto-labeddit/src/pages/FeedPage/styled.js
+++ modulo3/projeto-labeddit/src/pages/FeedPage/styled.js
@@ -1,10 +1,12 @@
-
+import Card from '@mui/material/Card';
 import Styled from 'styled-components'
 
 export const StyledDivFeed = Styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    word-break: break-all;
+    margin: 3%
 
 `
 export const StyledCard = Styled.div`
@@ -12,5 +14,7 @@ export const StyledCard = Styled.div`
     flex-direction: column;
     align-items: center;
 
+`
+export const StyledCardMui = Styled(Card)`
 
 `
